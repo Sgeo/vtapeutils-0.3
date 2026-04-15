b40dcd9ae41a1b519758367ef80beac11790e5a6
Sgeo <sgeoster@gmail.com>
2026-04-15 01:37:29 -0400

    Forgot to add p7b.h

---
+++ p7b.h
@@ -0,0 +1,10 @@
+#define P7B_PARITY 0x40
+#define P7B_START 0x80
+#define P7B_TAPE_MARK  0x0F
+
+
+/* Functions. */
+int p7b_read(VTAPE_FILE *infile, unsigned char *buffer, unsigned int maxlen);
+int p7b_write(VTAPE_FILE *outfile, unsigned char *buffer, unsigned int reclength);
+int p7b_open(VTAPE_FILE *file, char *filename, char *mode);
+int p7b_close(VTAPE_FILE *file);
